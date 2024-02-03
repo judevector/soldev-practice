c8d1fc170676a7c762809f2e0dda1ee89bf1adfa
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-02-03 22:50:58 +0400

    Data bundling and transfer

--- src/transfer-data.ts
+++ src/transfer-data.ts
@@ -11,20 +11,19 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 const suppliedToPubkey = process.argv[2] || null;
-const solAmount = process.argv[3];
+const suppliedToPubkey2 = process.argv[3] || null;
+const solAmount = process.argv[4];
 const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
 const connection = new Connection(clusterApiUrl("devnet"));
+const toPubkey = new PublicKey(suppliedToPubkey);
+const toPubkey2 = new PublicKey(suppliedToPubkey2);
+const transaction = new Transaction();
+const SOL_AMOUNT = parseInt(solAmount) * LAMPORTS_PER_SOL;
 
 if (!suppliedToPubkey) {
   console.log(`Please provide a public key to send to`);
   process.exit(1);
 }
-
-const toPubkey = new PublicKey(suppliedToPubkey);
-
-const transaction = new Transaction();
-
-const SOL_AMOUNT = parseInt(solAmount) * LAMPORTS_PER_SOL;
 
 const sendSolInstruction = SystemProgram.transfer({
   fromPubkey: senderKeypair.publicKey,
@@ -32,9 +31,16 @@ const sendSolInstruction = SystemProgram.transfer({
   lamports: SOL_AMOUNT,
 });
 
+const sendSolInstruction2 = SystemProgram.transfer({
+  fromPubkey: senderKeypair.publicKey,
+  toPubkey: toPubkey2,
+  lamports: SOL_AMOUNT,
+});
+
 transaction.add(sendSolInstruction);
+transaction.add(sendSolInstruction2);
 
 const signature = await sendAndConfirmTransaction(connection, transaction, [senderKeypair]);
 
-console.log(`💸 Finished! Sent ${solAmount} SOL to the address ${toPubkey}. `);
+console.log(`💸 Finished! Sent ${solAmount} SOL to the address ${toPubkey} and ${toPubkey2}. `);
 console.log(`Transaction signature is ${signature}`);
