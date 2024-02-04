32bb8ed150009d7e4387833360cfefb2e7f3d884
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-02-05 02:20:26 +0400

    Sending Ping transactions

--- src/generate-keypair.ts
+++ src/generate-keypair.ts
@@ -1,6 +1,13 @@
 require("dotenv").config();
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import base58 from "bs58";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
+const secretKeyBuffer = Buffer.from(keypair.secretKey);
+const privatekey = base58.encode(secretKeyBuffer);
+const publickey = keypair.publicKey;
+
+console.log(`Public key: ${publickey}`);
+console.log(`Private key: ${privatekey}`);
 
 console.log(`✅ Finished! We've loaded our secret key securely, using an env file!`);
